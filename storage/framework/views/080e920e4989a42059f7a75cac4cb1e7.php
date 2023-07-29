a2d8a97eee20b9850c8467a0436809f5253a2100
Hoang Ca <accclonek25@gmail.com>
2023-07-29 10:25:53 +0700

    update

--- storage/framework/views/080e920e4989a42059f7a75cac4cb1e7.php
+++ storage/framework/views/080e920e4989a42059f7a75cac4cb1e7.php
@@ -45,6 +45,21 @@
                                 <p>... MGMT</p>
                             </a>
                         </li>
+                        <li class="nav-item" id="other" class="<?php echo e(request()->is('...') ? 'active' : ''); ?>">
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser"
+                                aria-expanded="true" aria-controls="collapseUser">
+                                <i class="fas fa-fw fa-cog"></i>
+                                <span>User management</span>
+                            </a>
+                            <div id="collapseUser" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                                <div class="bg-white py-2 collapse-inner rounded">
+                                    <h6 class="collapse-header">All Functions</h6>
+                                    <a class="collapse-item" href="<?php echo e(asset('Admins/users/listuser')); ?>">List</a>
+                                    <a class="collapse-item" href="<?php echo e(asset('Admins/users/manageusers')); ?>">Integrated management
+                                    </a>                    
+                                </div>
+                            </div>
+                        </li>
                         
                     <?php endif; ?>
 
