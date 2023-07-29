aae2fac32bcd0e35f8c1de1b7883f11fc1b74ca4
Ha Cong Minh <minhhacong883@gmail.com>
2023-07-29 09:39:07 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,8 +24,20 @@ class LoginController extends Controller
             // Authentication successful
             $user = Auth::user();
 
-            // Redirect to the dashboard route, passing the username as a query parameter
-            return redirect()->route('All.Dashboard', ['username' => $user->role]);
+            // Redirect based on the user's role
+            switch ($user->role) 
+            {
+                case 'admin':
+                    return redirect()->route('All.index.admin');
+                case 'training':
+                    return redirect()->route('All.index.training');
+                case 'trainer':
+                    return redirect()->route('All.index.trainer');
+                    // Add more cases for other roles if needed
+                default:
+                    // Redirect to a default page or dashboard if role not matched
+                    return redirect()->route('Auth.getLogin');
+            }
         } else {
             // Authentication failed, redirect back to login with error message
             return redirect()->back()->withErrors([
@@ -33,37 +45,5 @@ class LoginController extends Controller
             ]);
         }
     }
-
-
-    // public function postLogin(Request $request)
-    // {
-    //     $credentials = $request->validate([
-    //         'email' => 'required|email',
-    //         'password' => 'required',
-    //     ]);
-
-    //     if (Auth::attempt($credentials)) {
-    //         // Authentication successful
-    //         $user = Auth::user();
-
-    //         // Redirect based on the user's role
-    //         switch ($user->role) 
-    //         {
-    //             case 'admin':
-    //                 return redirect()->route('All.index.admin');
-    //             case 'training':
-    //                 return redirect()->route('All.index.training');
-    //             case 'trainer':
-    //                 return redirect()->route('All.index.trainer');
-    //                 // Add more cases for other roles if needed
-    //             default:
-    //                 // Redirect to a default page or dashboard if role not matched
-    //                 return redirect()->route('Auth.getLogin');
-    //         }
-    //     } else {
-    //         // Authentication failed, redirect back to login with error message
-    //         return redirect()->back()->withErrors([
-    //             'email' => 'Invalid credentials',
-    //         ]);
-    //     }
+    
 }
